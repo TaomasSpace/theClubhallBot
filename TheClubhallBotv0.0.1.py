d229b4d3e1f2d1ef5b39bb38c647ef10bd660e8f
DanielMuerner <155637174+DanielMuerner@users.noreply.github.com>
2025-04-20 20:17:21 +0200

    made stab gif cleaner and easier to add/delete gifs

--- TheClubhallBotv0.0.1.py
+++ TheClubhallBotv0.0.1.py
@@ -45,12 +45,6 @@ def init_db():
             max_coins INTEGER
         )
     ''')
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS stab_gifs (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            url TEXT NOT NULL
-        )
-    ''')
     cursor.execute('INSERT OR IGNORE INTO server (id, max_coins) VALUES (1, ?)', (DEFAULT_MAX_COINS,))
     cursor.execute('UPDATE users SET money = 5 WHERE money = 0')
     conn.commit()
@@ -106,17 +100,6 @@ def set_max_coins(new_limit):
 
 def has_role(member: discord.Member, role_name):
     return any(role.name == role_name for role in member.roles)
-
-def get_random_stab_gif():
-    conn = sqlite3.connect(DB_PATH)
-    cursor = conn.cursor()
-    cursor.execute('SELECT url FROM stab_gifs')
-    results = cursor.fetchall()
-    conn.close()
-    if results:
-        return choice(results)[0]
-    else:
-        return None
 
 # === REQUEST BUTTON VIEW ===
 class RequestView(ui.View):
@@ -352,6 +335,36 @@ async def stab(interaction: discord.Interaction, user: discord.Member):
         "https://i.makeagif.com/media/12-15-2017/I2zZ0u.gif",
         "https://media.tenor.com/2pCPJqG46awAAAAM/yes-adventure-time.gif"
     ]
+    
+    stab_gifs = [
+        "https://i.gifer.com/EJEW.gif"
+        "https://i.makeagif.com/media/9-04-2024/cpYT-t.gif"
+        "https://64.media.tumblr.com/7e2453da9674bcb5b18a2b60795dfa07/6ffe034dc5a009bc-9b/s540x810/8eac80d1085f4d86de5c884ab107e79c6f16fc8c.gif"
+        "https://i.pinimg.com/originals/94/c6/37/94c6374c19b627b3a9101f4f9c0585f7.gif"
+        "https://i.gifer.com/IlQa.gif"
+        "https://i.pinimg.com/originals/dc/ac/b2/dcacb261a7ed5b4f368134795017b12f.gif"
+        "https://64.media.tumblr.com/tumblr_m7df09awmi1qbvovho1_500.gif"
+        "https://i.imgur.com/S0TeWYF.gif"
+        "https://i.makeagif.com/media/11-05-2023/mFVJ4J.gif"
+        "https://pa1.aminoapps.com/6253/6fce33471439acfba14932815cc111d617d1ccc4_hq.gif"
+        "https://64.media.tumblr.com/71996944f95f102c6bedc0191d8935d3/16b46dc7e02ca5dd-6d/s500x750/bbaee60ca59a9b5baea445b6440bd77e6f93f9c3.gif"
+        "https://giffiles.alphacoders.com/732/73287.gif"
+        "https://gifdb.com/images/high/haruhi-ryoko-asakura-stabbing-kyon-wv9iij0gq6khu6px.gif"
+        "https://gifdb.com/images/high/kill-sekai-saionji-school-days-stabbing-anime-br2876uawee0jzus.gif"
+        "https://gifdb.com/images/high/black-butler-stab-7n1xy2127wzi18sk.gif"
+        "https://i.gyazo.com/1fdb81ac3ebfb2dee9af9b4127760b70.gif"
+        "https://pa1.aminoapps.com/5765/b867c3725e281743e173dc86340c76733281d07f_hq.gif"
+        "https://64.media.tumblr.com/003a4f007486f7963f29edcc454425d4/4ff62cb12f9f72e0-51/s540x810/3cb10ed0bdd781a9b5b0864e833f448ece0efed2.gif"
+        "https://i.gifer.com/GeUx.gif"
+        "https://i.makeagif.com/media/7-19-2018/NFSY1t.gif"
+        "https://i.makeagif.com/media/4-17-2015/ba-nRJ.gif"
+        "https://i.makeagif.com/media/11-12-2023/a3YlZs.gif"
+        "https://i.redd.it/rq7v0ky0q9wb1.gif"
+        "https://i.imgur.com/HifpF82.gif"
+        "https://images.squarespace-cdn.com/content/v1/5b23e822f79392038cbd486c/1561132763571-XTIOP1FRN1OJRWSGZ9F1/tumblr_o5lpjy37HN1v7kio1o1_500.gif"
+        "https://i.makeagif.com/media/11-18-2022/Ezac_n.gif"
+        "https://c.tenor.com/uc9Qh0p1mOIAAAAC/yuno-gasai-mirai-nikki.gif"
+            ]
 
     sender_id = interaction.user.id
     try:
@@ -377,7 +390,7 @@ async def stab(interaction: discord.Interaction, user: discord.Member):
         if has_role(interaction.user, OWNER_ROLE_NAME):
             chance = 0.90
         if random() < chance:
-            gif_url = get_random_stab_gif()
+            gif_url = choice(stab_gifs)
             if gif_url:
                 embed = discord.Embed(title=f"{interaction.user.name} stabs {user.name}!", color=discord.Color.red())
                 embed.set_image(url=gif_url)
