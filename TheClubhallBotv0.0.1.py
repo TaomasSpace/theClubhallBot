80f6a58603b6cde826103e403d9bd74e942c837c
DanielMuerner <daniel.muerner@bluewin.ch>
2025-06-03 10:06:31 +0200

    added prison command

--- TheClubhallBotv0.0.1.py
+++ TheClubhallBotv0.0.1.py
@@ -2137,6 +2137,36 @@ async def grantrole(interaction: discord.Interaction, target: discord.Member):
 
 
 @bot.tree.command(
+    name="manageprisonmember", description="Send or free someone from prison"
+)
+@app_commands.describe(user="Person you want to lock or free in prison")
+async def managePrisonMember(interaction: discord.Interaction, user: discord.Member):
+    if not has_role(interaction.user, ADMIN_ROLE_NAME):
+        await interaction.response.send_message("No permission.", ephemeral=True)
+        return
+
+    role_name = "Guest of the Clubhall"
+    role = discord.utils.get(interaction.guild.roles, name=role_name)
+
+    if not role:
+        await interaction.response.send_message(
+            f"❌ Role '{role_name}' not found.", ephemeral=True
+        )
+        return
+
+    if role in user.roles:
+        await user.remove_roles(role)
+        await interaction.response.send_message(
+            f"🔓 {user.mention} has been freed from prison.", ephemeral=False
+        )
+    else:
+        await user.add_roles(role)
+        await interaction.response.send_message(
+            f"🔒 {user.mention} has been sent to prison.", ephemeral=False
+        )
+
+
+@bot.tree.command(
     name="addcolorreactionrole", description="Add emoji-role to color reaction message"
 )
 @app_commands.describe(emoji="Emoji to react with", role="Role to assign")
